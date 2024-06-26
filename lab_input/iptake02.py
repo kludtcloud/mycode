ba10d440203220d05f17d7e94b01afa3fd3d90fd
kludtcloud <kludtcloud@gmail.com>
2024-06-26 19:16:08 +0000

    add comments

--- lab_input/iptake02.py
+++ lab_input/iptake02.py
@@ -12,10 +12,12 @@ slot = 0
 
 print("Level: ", trainlevel)
 print("Poke' slots: ", slot)
+#not sure whats happening here: fix sep line and +lost value
 print("Battles (W/L): ", won + lost sep = "/")
 
+#print options for next: list options and then changes those values based on inputs
 print("What would you like to do:")
-input("Help, Battle, Stats" '\n')
+input("Help, Battle, Stats, Set Poke Slot" '\n')
 
 
 
