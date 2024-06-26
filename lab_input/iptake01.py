d5f978e5a966f55041bc1d8d37c1c877ab62dbab
kludtcloud <kludtcloud@gmail.com>
2024-06-26 19:12:52 +0000

    Learning about input functions

---
+++ lab_input/iptake01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+user_input = input ("Please enter an IPv4 IP address:")
+print("You told me the IPv4 is:" + user_input)
+
+    # the + is Concatenation, join strings together)
+
---
+++ lab_input/iptake02.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+trainer = input("Please enter trainer name:")
+won = 0
+lost = 0
+
+print("Congradulations you are: Trainer " + trainer)
+
+#set user stats and print
+trainlevel = 1
+slot = 0
+
+print("Level: ", trainlevel)
+print("Poke' slots: ", slot)
+print("Battles (W/L): ", won + lost sep = "/")
+
+print("What would you like to do:")
+input("Help, Battle, Stats" '\n')
+
+
+
+
