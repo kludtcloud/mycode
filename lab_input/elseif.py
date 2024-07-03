045c2ace0b2e09f236e37b3c8cda68916c0c9225
kludtcloud <kludtcloud@gmail.com>
2024-07-03 22:46:28 +0000

    errors fixed

--- lab_input/elseif.py
+++ lab_input/elseif.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env Python3
 
-lenght_int()
+lenght_int = ""
+target_lenght = 256
 
 user_input = input("Generate key, enter characters:" '\n')
 lenght_int += user_input
 
 
-if len(lenght_int) > target_length:256
-input_int = input_string[:target_length]
-print("Key generated")
-print(f"Key: {lenght_int}")
-else
-lenght_int() += input("keep going:" '\n').strip()
+if: len(lenght_int) > target_length
+    lenght_int = lenght_string[:target_length]
+    print("Key generated")
+    print(f"Key: {lenght_int}")
+else:
+    lenght_int() += input("keep going:" '\n').strip()
